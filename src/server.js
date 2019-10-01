900d6a6b6029bc7352ee48219636fab8ab595387
João Victor da Costa Gonçalves <joaog@kyros.com.br>
2019-10-01 15:42:47 -0300

    starting project

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-3spkj.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+PORT = 3333
+app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
